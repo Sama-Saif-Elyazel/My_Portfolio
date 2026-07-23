e64bf54f6b8554b7a395082bfd9cada3fda19a06
Sama <Semsma.ana8090100@gmail.com>
2026-07-23 20:28:11 +0200

    Update portfolio content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,12 +17,12 @@ export default function Navbar() {
                     <a href="#contact" className='text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer'>Contact</a>
                     <a href="#certificates" className='text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer'>Certificates</a>
                 </div>
-                <button className='md:hidden text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer' onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label="Toggle Menu">
+                <button className='fixed right-10 top-10 md:hidden text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer' onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label="Toggle Menu">
                     {isMenuOpen ? 'Close' : 'Menu'}
                 </button>
             </div>
             {isMenuOpen && (
-                <div className='md:hidden bg-[#4c0a5e] text-[#db76ea] font-[cursive] flex flex-col items-center gap-4 py-4 overflow-x-hidden max-w-full'>
+                <div className='fixed right-5 top-25 z-50 md:hidden bg-[#4c0a5e] text-[#db76ea] font-[cursive] flex flex-col items-center gap-4 py-4 overflow-x-hidden max-w-full'>
                     <a href="#home" className='hover:text-[#e7cae8] hover:cursor-pointer'>Home</a>
                     <a href="#about" className='hover:text-[#e7cae8] hover:cursor-pointer'>About Me</a>
                     <a href="#skills" className='hover:text-[#e7cae8] hover:cursor-pointer'>Skills</a>

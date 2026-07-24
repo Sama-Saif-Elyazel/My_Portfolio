7edf5650ee06898b0c6693b83b83f23fe646b137
Sama <Semsma.ana8090100@gmail.com>
2026-07-24 23:32:05 +0200

    Update portfolio content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,20 @@ import React from 'react'
 export default function About() {
     return (
         <>
-            <div className='bg-[#ecd7ef] min-h-screen w-full relative flex flex-col items-center justify-center gap-20 overflow-x-hidden max-w-full scroll-mt-24' id='about'>
+            <div className='bg-[#ecd7ef] min-h-screen w-full relative flex flex-col items-center justify-center overflow-x-hidden max-w-full scroll-mt-24 py-5 px-4 pt-0' id='about'>
                 <img src="./assets/images/Screenshot (67).png" className='absolute inset-0 z-0 w-full h-full object-cover opacity-80 ' />
-                <div className='relative z-10 flex flex-col items-center justify-center text-center gap-5 p-20'>
-                    <h1 className=' text-6xl mid:text-6xl font-bold text-[#cfa4db] font-[cursive] pt-20'>About Me</h1>
-                   
-                    <div className='flex flex-row'>
-                        <img src="./assets/images/samsom.jpeg" className='w-1/2 h-auto flex col-span-1 object-cover z-9' style={{ borderRadius: '60%' }} />
-                        <div className='flex flex-col justify-center items-center gap-5'>
-                            
-                            <h1 className='text-5xl font-bold text-[#cfa4db] font-[cursive]'>Hello, I'm Sama Saif Elyazel</h1>
-                            <h1 className='text-6xl font-bold text-[#cfa4db] font-[cursive]'>Software Engineer</h1>
+                <div className='relative z-10 w-full max-w-7xl mx-auto flex flex-col items-center gap-12 px-6 lg:px-12'>
+                    <h1 className=' text-5xl mid:text-6xl font-bold text-[#e1c8e9] font-[cursive] drop-shadow-md pt-6 text-center'>About Me</h1>
+
+                    <div className='flex flex-row md:flex-row items-center justify-center md:justify-between gap-10 md:gap-16 lg:gap-15 w-full px-12 md:px-0 my-auto'>
+                       
+                        <div className='w-60 h-80 sm:w-72 sm:h-100 md:w-80 md:h-110 shrink-0 overflow-hidden rounded-full shadow-2xl border-4 border-[#e1c8e9] '>
+                        <img src="./assets/images/samsom.jpeg" className='w-full h-full object-cover z-9' style={{ borderRadius: '70% / 50%' }} />
+                        </div>
+
+                        <div className='flex flex-col justify-center items-center md:items-end text-center md:text-right gap-10 max-w-2xl px-4 md:px-0'>
+                            <h2 className='text-4xl sm:text-4xl lg:text-5xl font-bold text-[#e1c8e9] font-[cursive] leading-tight drop-shadow-md'>Hello, <br></br> I'm Sama Saif Elyazel</h2>
+                            <h3 className='text-2xl sm:text-3xl lg:text-6xl font-bold text-[#e1c8e9] font-[cursive] leading-tight drop-shadow-md'>Software Engineer</h3>
                         </div>
                     </div>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     return (
         <>
-            <div className='fixed top-0 bg-[#4c0a5e] h-25 w-full flex items-center justify-between px-4 z-50 overflow-x-hidden max-w-full'>
+            <div className='fixed top-0 bg-[#4c0a5e] h-25 w-full flex items-center justify-between px-2 z-50 gap-6 overflow-x-hidden max-w-full'>
                 <div>
                     <h1 className='text-4xl font-bold text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer whitespace-nowrap'>Sama Saif Elyazel</h1>
                 </div>
-                <div className='text-xl gap-8 hidden md:flex overflow-x-hidden max-w-full'>
+                <div className='text-xl gap-5 hidden md:flex overflow-x-hidden max-w-full'>
                     <a href="#home" className='text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer'>Home</a>
                     <a href="#about" className='text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer'>About Me</a>
                     <a href="#skills" className='text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer'>Skills</a>
@@ -20,17 +20,18 @@ export default function Navbar() {
                 <button className='fixed right-10 top-10 md:hidden text-[#db76ea] font-[cursive] hover:text-[#e7cae8] hover:cursor-pointer' onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label="Toggle Menu">
                     {isMenuOpen ? 'Close' : 'Menu'}
                 </button>
+
+                {isMenuOpen && (
+                    <div className='fixed right-5 top-25 z-50 md:hidden bg-[#4c0a5e] text-[#db76ea] font-[cursive] flex flex-col items-center gap-4 py-4 overflow-x-hidden max-w-full'>
+                        <a href="#home" className='hover:text-[#e7cae8] hover:cursor-pointer'>Home</a>
+                        <a href="#about" className='hover:text-[#e7cae8] hover:cursor-pointer'>About Me</a>
+                        <a href="#skills" className='hover:text-[#e7cae8] hover:cursor-pointer'>Skills</a>
+                        <a href="#projects" className='hover:text-[#e7cae8] hover:cursor-pointer'>Projects</a>
+                        <a href="#contact" className='hover:text-[#e7cae8] hover:cursor-pointer'>Contact</a>
+                        <a href="#certificates" className='hover:text-[#e7cae8] hover:cursor-pointer'>Certificates</a>
+                    </div>
+                )}
             </div>
-            {isMenuOpen && (
-                <div className='fixed right-5 top-25 z-50 md:hidden bg-[#4c0a5e] text-[#db76ea] font-[cursive] flex flex-col items-center gap-4 py-4 overflow-x-hidden max-w-full'>
-                    <a href="#home" className='hover:text-[#e7cae8] hover:cursor-pointer'>Home</a>
-                    <a href="#about" className='hover:text-[#e7cae8] hover:cursor-pointer'>About Me</a>
-                    <a href="#skills" className='hover:text-[#e7cae8] hover:cursor-pointer'>Skills</a>
-                    <a href="#projects" className='hover:text-[#e7cae8] hover:cursor-pointer'>Projects</a>
-                    <a href="#contact" className='hover:text-[#e7cae8] hover:cursor-pointer'>Contact</a>
-                    <a href="#certificates" className='hover:text-[#e7cae8] hover:cursor-pointer'>Certificates</a>
-                </div>
-            )}
         </>
     )
 }

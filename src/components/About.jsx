71b23b9176bd02a20a1f32e4d9fe87a6c6247525
Sama <Semsma.ana8090100@gmail.com>
2026-07-25 14:24:45 +0200

    Update portfolio content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function About() {
     return (
         <>
-            <div className='bg-[#ecd7ef] min-h-screen w-full relative flex flex-col items-center justify-center overflow-x-hidden max-w-full scroll-mt-24 py-5 px-4 pt-0' id='about'>
+            <div className='bg-[#ecd7ef] min-h-screen w-full relative flex flex-col items-center justify-center overflow-x-hidden max-w-full scroll-mt-24 py-5 px-0 pt-0' id='about'>
                 <img src="./assets/images/Screenshot (67).png" className='absolute inset-0 z-0 w-full h-full object-cover opacity-80 ' />
                 <div className='relative z-10 w-full max-w-7xl mx-auto flex flex-col items-center gap-12 px-6 lg:px-12'>
                     <h1 className=' text-5xl mid:text-6xl font-bold text-[#e1c8e9] font-[cursive] drop-shadow-md pt-6 text-center'>About Me</h1>
 
-                    <div className='flex flex-row md:flex-row items-center justify-center md:justify-between gap-10 md:gap-16 lg:gap-15 w-full px-12 md:px-0 my-auto'>
+                    <div className='flex flex-row md:flex-row items-center justify-center md:justify-between gap-10 md:gap-16 lg:gap-15 w-full px-0 md:px-0 my-auto'>
                        
-                        <div className='w-60 h-80 sm:w-72 sm:h-80 md:w-70 md:h-110 shrink-0 overflow-hidden rounded-full shadow-2xl border-4 border-[#e1c8e9] '>
+                        <div className='w-60 h-80 sm:w-72 sm:h-60 md:w-50 md:h-110 shrink-0 overflow-hidden rounded-full shadow-2xl border-4 border-[#e1c8e9] '>
                         <img src="./assets/images/samsom.jpeg" className='w-full h-full object-cover z-9' style={{ borderRadius: '70% / 50%' }} />
                         </div>
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { FaFacebook, FaLinkedin } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
 import { FaSpotify } from "react-icons/fa"
-import { FaYoutube } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa";
 
 export default function Contact() {
   return (
@@ -19,14 +20,14 @@ export default function Contact() {
             <div className='flex flex-col justify-center items-center gap-5'>
               <h1 className='text-4xl font-bold text-[#4c0a5e] font-[cursive]'>Phone</h1>
               <p className='text-2xl text-[#4c0a5e] font-[cursive]'>01127836069</p>
-            </div>
+            </div> 
             <div>
               <h1 className='text-4xl font-bold text-[#4c0a5e] font-[cursive]'>Social Media</h1>
               <div className='flex flex-row gap-5 mt-5'>
                 <a href="https://www.facebook.com/semsma.ana.39/" target="_blank" rel="noopener noreferrer"><FaFacebook className='w-8 h-8 text-[#4c0a5e] hover:text-blue-900 hover:cursor-pointer' /></a>
                 <a href="https://www.instagram.com/sama_saif826?igsh=MWdqNmk4NWV6bWVkdw==" target="_blank" rel="noopener noreferrer"><FaInstagram className='w-8 h-8 text-[#4c0a5e] hover:text-pink-900 hover:cursor-pointer ml-5' /></a>
                 <a href="https://www.linkedin.com/in/sama-saif-356ba241b?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noopener noreferrer"><FaLinkedin className='w-8 h-8 text-[#4c0a5e] hover:text-blue-900 hover:cursor-pointer ml-5' /></a>
-                <a href="https://www.youtube.com/channel/UCXoQZtHkKzj3nLhXoV9r8w" target="_blank" rel="noopener noreferrer"><FaYoutube className='w-8 h-8 text-[#4c0a5e] hover:text-red-900 hover:cursor-pointer ml-5' /></a>
+                <a href="https://github.com/Sama-Saif-Elyazel" target="_blank" rel="noopener noreferrer"><FaGithub className='w-8 h-8 text-[#4c0a5e] hover:text-gray-900 hover:cursor-pointer ml-5' /></a>
               </div>
             </div>
           </div>
